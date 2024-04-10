e97d5cae1e13eed0faae33103119c15962146d62
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2024-04-10 08:34:58 +0200

    feat: add instruction sorting based on their order

--- student/Interpreter.php
+++ student/Interpreter.php
@@ -2,11 +2,49 @@
 
 namespace IPP\Student;
 
+use DOMElement;
+use DOMNode;
 use IPP\Core\AbstractInterpreter;
-use IPP\Core\Exception\NotImplementedException;
+use IPP\Core\ReturnCode;
+use IPP\Student\Exception\SemanticErrorException;
 
 class Interpreter extends AbstractInterpreter
 {
+    /** @var array<int> $labelPositions */
+    private $labelPositions;
+
+    private static function instruction_sort(DOMElement $a, DOMElement $b): int
+    {
+        if((int)$a->getAttribute('order') < 0 || (int)$b->getAttribute('order') < 0) {
+            throw new SemanticErrorException('ERROR: instruction with negative order');
+        }
+        return (int)$a->getAttribute('order') - (int)$b->getAttribute('order');
+    }
+    /**
+     * @param array<DOMElement> $instructionElements
+     */
+    private function firstPass($instructionElements): void
+    {
+        // Get label positions
+        foreach($instructionElements as $key => $el) {
+            if($el->getAttribute('opcode') == "LABEL") {
+                $label = $el->firstElementChild;
+                if(!is_null($label)) {
+                    $label = $label->nodeValue;
+                }
+
+                $this->labelPositions[$label] = $key;
+            }
+        }
+    }
+    /**
+     * @param DOMNode[] $instructionElements
+     */
+    private function secondPass($instructionElements): void
+    {
+
+    }
+
     public function execute(): int
     {
         // TODO: Start your code here
@@ -15,6 +53,20 @@ class Interpreter extends AbstractInterpreter
         // $val = $this->input->readString();
         // $this->stdout->writeString("stdout");
         // $this->stderr->writeString("stderr");
-        throw new NotImplementedException;
+
+        $context = new Context();
+        $dom = $this->source->getDOMDocument();
+        $gf = new Frame();
+
+        // Get sorted array of instructions
+        $instructionsDOMList = $dom->getElementsByTagName('instruction');
+        $instructionElements = iterator_to_array($instructionsDOMList);
+
+        usort($instructionElements, array($this, "instruction_sort"));
+
+        $this->firstPass($instructionElements);
+        $this->secondPass($instructionElements);
+
+        return ReturnCode::OK;
     }
 }
